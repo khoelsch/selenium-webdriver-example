4ea999ee4bd2f42937473343e5ea89f38f121c94
Kai Hölscher <kai.hoelscher@sabio.de>
2018-09-01 11:10:24 +0200

    Add Mockito dependency + Add unit test for WebElementAssert.java

--- TODO.md
+++ TODO.md
@@ -1,7 +1,7 @@
 # Open TODOs =
 
-0. Remove versions of Maven plugins in child-POMs(?)
 0. Add Maven Plugins: Checkstyle, PMD and Findbugs (and let build fail on errors)
+0. Generate code coverage information in Maven build
 0. Introduce sth. similar to page objects (see https://github.com/SeleniumHQ/selenium/wiki/PageFactory)
 0. Separate smoke tests from the rest 
 0. Use Java properties to handle localization(?)
---
+++ webdriver-utilities/src/test/java/de/edgeway/webdriver/utilities/assertions/WebElementAssertTest.java
@@ -0,0 +1,85 @@
+package de.edgeway.webdriver.utilities.assertions;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebElement;
+
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class WebElementAssertTest {
+
+    @Test
+    void isUsable_accept_valid_WebElement() {
+        WebElement validWebElement = mock(WebElement.class);
+
+        when(validWebElement.isDisplayed()).thenReturn(true);
+        when(validWebElement.isEnabled()).thenReturn(true);
+
+        // Should not throw an exception
+        WebElementAssert.assertThat(validWebElement).isUsable();
+    }
+
+    @Test
+    void isUsable_throwIf_WebElementIsDisabled() {
+        WebElement disabledWebElement = mock(WebElement.class);
+
+        when(disabledWebElement.isDisplayed()).thenReturn(true);
+        when(disabledWebElement.isEnabled()).thenReturn(false);
+
+        assertThatExceptionOfType(AssertionError.class).isThrownBy( () ->
+            WebElementAssert.assertThat(disabledWebElement).isUsable()
+        );
+    }
+
+    @Test
+    void isUsable_throwIf_WebElementIsNotDisplayed() {
+        WebElement notShownWebElement = mock(WebElement.class);
+
+        when(notShownWebElement.isEnabled()).thenReturn(true);
+        when(notShownWebElement.isDisplayed()).thenReturn(false);
+
+        assertThatExceptionOfType(AssertionError.class).isThrownBy( () ->
+            WebElementAssert.assertThat(notShownWebElement).isUsable()
+        );
+    }
+
+    @Test
+    void containsText_accept_valid_WebElement() {
+        WebElement validWebElement = mock(WebElement.class);
+
+        when(validWebElement.getText()).thenReturn("foobar");
+
+        // Should not throw an exception
+        WebElementAssert.assertThat(validWebElement).containsText("foo");
+        WebElementAssert.assertThat(validWebElement).containsText("bar");
+        WebElementAssert.assertThat(validWebElement).containsText("foobar");
+    }
+
+    @Test
+    void containsText_throwIf_text_is_null() {
+        WebElement invalidWebElement = mock(WebElement.class);
+
+        when(invalidWebElement.getText()).thenReturn(null);
+
+        assertThatExceptionOfType(AssertionError.class).isThrownBy( () ->
+            WebElementAssert.assertThat(invalidWebElement).containsText("foobar")
+        );
+    }
+
+    @Test
+    void containsText_throwIf_text_is_not_contained() {
+        WebElement invalidWebElement = mock(WebElement.class);
+
+        when(invalidWebElement.getText()).thenReturn("foobar");
+
+        assertThatExceptionOfType(AssertionError.class).isThrownBy( () ->
+            WebElementAssert.assertThat(invalidWebElement).containsText("foobarium")
+        );
+    }
+
+
+    //
+    // --- Helper methods ----------------------------------------------------------------------------------------------
+    //
+}
