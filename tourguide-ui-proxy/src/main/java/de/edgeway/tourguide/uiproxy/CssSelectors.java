9f82278b4ff06a4a29e22aca2991e2db03ba0107
Kai Hölscher <kai.hoelscher@sabio.de>
2018-09-09 22:45:28 +0200

    Move file

---
+++ tourguide-ui-proxy/src/main/java/de/edgeway/tourguide/uiproxy/CssSelectors.java
@@ -0,0 +1,12 @@
+package de.edgeway.tourguide.uiproxy;
+
+/**
+ * Enumerates CSS selectors of web page elements that are relevant for the UI tests.
+ */
+public class CssSelectors {
+  public static final String FOOTER = ".footer-content";
+  public static final String USER_PROFILE_NAME_CONTAINER = ".profile-name";
+  public static final String MAIN_MENU_BUTTON = "ul.layout-tabmenu-nav a.tabmenuitem-link";
+  public static final String MAIN_MENU = "ul.navigation-menu";
+  public static final String MAIN_MENU_ENTRIES = "ul.navigation-menu a span";
+}
